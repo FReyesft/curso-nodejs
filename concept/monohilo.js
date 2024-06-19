d4eab5ea7a53925c3c853205ac82479f84675df0
FReyesft <f.reyesft@gmail.com>
2024-06-19 16:26:55 -0500

    monohilo y posibles problemas con los errores en node

---
+++ concept/monohilo.js
@@ -0,0 +1,10 @@
+//* Cuando ocurre un error en alguno de los hilos Node detiene todos los hilos de ejecución.
+console.log('¿Hola como estan?');
+let count = 0;
+setInterval(() => {
+	console.log(count)
+	count++
+	if(count === 5) {
+		var a = 3 + t
+	}
+}, 1000)
